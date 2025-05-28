88046808c65bfae89134adb24c4b3f7d88ae2862
khalidadel365 <95598763+khalidadel365@users.noreply.github.com>
2025-05-28 03:39:16 +0300

    change theme and create notes view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -5,6 +5,10 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text('Notes View'),
+      ),
+    );
   }
 }
