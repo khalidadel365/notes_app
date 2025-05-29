3c79634385416a62577d7f98374933a6139eac7c
khalidadel365 <95598763+khalidadel365@users.noreply.github.com>
2025-05-29 04:27:25 +0300

    edit not view ui

--- lib/widgets/edit_view_body.dart
+++ lib/widgets/edit_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_text_form_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -13,7 +14,21 @@ class EditNoteViewBody extends StatelessWidget {
           SizedBox(height: 50,),
           CustomAppBar(
             title: 'Edit Note',
-            icon: Icons.check,)
+            icon: Icons.check,),
+          SizedBox(height: 50,),
+          CustomTextFormField(
+            hintText: 'Title',
+            maxLines: 1,
+            maxLength: 50,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextFormField(
+            hintText: 'Content',
+            maxLines: 5,
+            maxLength: 180,
+          ),
         ],
       ),
     );
